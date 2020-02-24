5fab2f81ee101878e66b1d72f974925cff65a912
han-yan-ds <hanprogramming@gmail.com>
2020-02-24 09:34:44 -0700

    Set up for typescript

---
+++ index.ts
@@ -0,0 +1,14 @@
+const PORT = 4000;
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(PORT, () => `Server is listening on port ${PORT}`);
